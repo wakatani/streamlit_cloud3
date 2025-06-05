7d6acf59a3f6be7800d94fedb40578da11f67cfd
Akiyoshi Wakatani <wakatani@konan-u.ac.jp>
2025-06-05 12:53:45 +0900

    June

--- GPTapp.py
+++ GPTapp.py
@@ -85,7 +85,7 @@ if st.button('問題'):
 
 if 'quiz' in st.session_state:
   quiz_response=st.session_state['quiz']
-  explanationst.session_state['expl'] 
+  explanation=st.session_state['expl'] 
   msg=quiz_response
   b[0]="１：{0}".format(quiz_response["選択肢１"])
   b[1]="２：{0}".format(quiz_response["選択肢２"])
